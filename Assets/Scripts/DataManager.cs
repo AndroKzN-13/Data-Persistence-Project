c6bf04cc230caa1efae591db15216daa45a9bb53
Febryanto <febryanto02.fa@gmail.com>
2026-01-26 00:15:16 +0700

    Start Menu
    - Start menu finished.

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.IO;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager Instance;
+    public string PlayerName;
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadName();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public string PlayerName;
+    }
+
+    public void SaveName()
+    {
+        SaveData data = new SaveData();
+        data.PlayerName = PlayerName;
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadName()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if(File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            PlayerName = data.PlayerName;
+        }
+    }
+}
---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public string PlayerName;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void HighScorePage()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void SettingPage()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Quit()
+    {
+        DataManager.Instance.SaveName();
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit;
+        #endif
+    }
+}
